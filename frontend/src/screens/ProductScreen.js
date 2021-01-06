887762b8a29413aba74df7be28cc57d64af1cee3
Rogino Salcedo <roginosalcedo@gmail.com>
2021-01-06 00:27:33 -0500

    started backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Product from "../components/Product";
 import Rating from "../components/Rating";
 import data from "../data";
 
